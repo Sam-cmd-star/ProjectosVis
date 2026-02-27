2d5395c9264111c50301e082c09a150cedbd9cc7
Sam-cmd-star <darkkrisalix616@gmail.com>
2026-02-27 17:14:29 -0500

    Deleted

--- EcoCleanManager/app/src/main/java/com/example/ecocleanmanager/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ecocleanmanager
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [ResiduoEntity::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun residuoDao(): ResiduoDao
-}
